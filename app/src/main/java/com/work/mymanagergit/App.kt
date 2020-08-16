9c2a105abdba1e3638f076d0b3cdfdae0d111bd1
Parkduksung <duksung1234@naver.com>
2020-08-16 23:22:35 +0900

    init-project

---
+++ app/src/main/java/com/work/mymanagergit/App.kt
@@ -0,0 +1,6 @@
+package com.work.mymanagergit
+
+import android.app.Application
+
+class App : Application() {
+}
---
+++ app/src/main/java/com/work/mymanagergit/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.work.mymanagergit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
